4d978c9c4984baba1ebddd1c575021c1e86081ef
German Fernandez <germanfernandez0306@gmail.com>
2022-03-16 20:05:19 -0300

    feat: change, add events

--- server/main.lua
+++ server/main.lua
@@ -13,19 +13,35 @@ local function add(status, value)
   player.addStatus(status, value)
 end
 
-local function update()
+local function subtract(status, value)
   local playerId = source
   local player = ATL.GetPlayer(playerId)
+
+  if not player.status[status] or status == 'health' or status == 'armor' then
+    return
+  end
+
+  if type(value) ~= 'number' or value > 100 or value <= 0 then
+    return
+  end
+
+  player.subtractStatus(status, value)
+end
+
+local function removeInterval()
+  local playerId = source
+  local player = ATL.GetPlayer(playerId)
+  local playerPed = GetPlayerPed(playerId)
   local status = {}
 
   for name, data in pairs(player.status) do
     if name == 'health' then
       status[name] = {
-        value = GetEntityHealth(GetPlayerPed(playerId)),
+        value = GetEntityHealth(playerPed),
       }
     elseif name == 'armor' then
       status[name] = {
-        value = GetPedArmour(GetPlayerPed(playerId)),
+        value = GetPedArmour(playerPed),
       }
     else
       local value = player.getStatus(name).value
@@ -47,5 +63,6 @@ local function update()
   player.setStatus(status)
 end
 
-RegisterNetEvent('atl-status:server:update', update)
+RegisterNetEvent('atl-status:server:removeInterval', removeInterval)
 RegisterNetEvent('atl-status:server:add', add)
+RegisterNetEvent('atl-status:server:subtract', subtract)
